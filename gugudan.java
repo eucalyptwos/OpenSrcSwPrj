46fd01f41cdd42ce70261c27845d329e9d564546
eucal <2468abcde@naver.com>
2020-04-24 18:52:55 +0900

    V2.0

--- gugudan.java
+++ gugudan.java
@@ -7,9 +7,13 @@ public class gugudan {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		int k = s.nextInt();
-		for( int i=1; i<=9; i++) {
-			System.out.println(k+"*"+i+"="+k*i);
-		}
+		if(k==0)
+			for(int j=1;j<=9;j++)
+				for( int i=1; i<=9; i++)
+					System.out.println(j+"*"+i+"="+j*i);
+		else
+			for( int i=1; i<=9; i++)
+				System.out.println(k+"*"+i+"="+k*i);
 	}
 
 }
